7ac6700da46bac2482a8f90b4365089b490c6f3b
Steffen Jørgensen <dk.steffen@gmail.com>
2022-12-06 14:41:08 +0100

    Add POC project

---
+++ MyService.Client/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Connecting to service...");
+
+var client = new ServiceReference1.ServiceClient(ServiceReference1.ServiceClient.EndpointConfiguration.BasicHttpBinding_IService);
+
+while (true)
+{
+    Console.WriteLine("Enter data to send to service:");
+    var input = Console.ReadLine();
+
+    var response = await client.GetDataAsync(input);
+    Console.WriteLine($"{response}");
+}
